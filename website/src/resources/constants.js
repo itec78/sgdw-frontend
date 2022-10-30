72e0d3e619465d19454d25a74ff885fc31e207dd
odoacre <0d0acre@esiliati.org>
2022-10-31 00:35:49 +0100

    Update constants

--- website/src/resources/constants.js
+++ website/src/resources/constants.js
@@ -1,4 +1,4 @@
-const LANG = process.env.LANG; // Language Code ISO_639
+const WIKI_LANG = process.env.WIKI_LANG || "it"; // Language Code ISO_639
 
-export const SDOW_API_URL = process.env.SDOW_API_URL;
-export const WIKIPEDIA_API_URL = `https://${LANG}.wikipedia.org/w/api.php`;
+export const SDOW_API_URL = process.env.SDOW_API_URL || "https://api.seigradidiwikipedia.it";
+export const WIKIPEDIA_API_URL = `https://${WIKI_LANG}.wikipedia.org/w/api.php`;
